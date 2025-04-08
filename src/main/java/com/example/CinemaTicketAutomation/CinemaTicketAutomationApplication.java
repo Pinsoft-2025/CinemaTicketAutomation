c27d1831dcef31c0a9b84fca03633325ecf101d7
SenaUstun-Dev <senaustun.dev@gmail.com>
2025-04-08 15:22:07 +0300

    project created

---
+++ src/main/java/com/example/CinemaTicketAutomation/CinemaTicketAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.example.CinemaTicketAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CinemaTicketAutomation/CinemaTicketAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CinemaTicketAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
