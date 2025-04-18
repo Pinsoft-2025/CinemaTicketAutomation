7fa0732008de61ae315fcad7dffb39ffba46c013
SenaUstun-Dev <senaustun.dev@gmail.com>
2025-04-18 15:20:17 +0300

    doc:added more info to UserCreateDto

--- src/main/java/com/example/CinemaTicketAutomation/dto/request/UserCreateDto.java
+++ src/main/java/com/example/CinemaTicketAutomation/dto/request/UserCreateDto.java
@@ -1,12 +1,14 @@
 package com.example.CinemaTicketAutomation.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Kullanıcı oluşturma isteği")
 public record UserCreateDto(
-        String username,
-        String password,
-        String mail,
-        String phoneNumber,
-        String firstname,
-        String lastname,
-        int age
-) {
-}
+        @Schema(description = "Kullanıcının kullanıcı adı") String username,
+        @Schema(description = "Şifre") String password,
+        @Schema(description = "Kullanıcının e-posta adresi") String mail,
+        @Schema(description = "Telefon numarası") String phoneNumber,
+        @Schema(description = "İsim") String firstname,
+        @Schema(description = "Soyisim") String lastname,
+        @Schema(description = "Yaş") Integer age
+) {}
