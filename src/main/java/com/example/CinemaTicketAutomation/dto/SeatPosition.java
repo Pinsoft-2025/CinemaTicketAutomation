851e9f6320dd8bcd61c68860aebc46041d6a792a
SenaUstun-Dev <senaustun.dev@gmail.com>
2025-04-18 15:20:41 +0300

    style:more readble SeatPosition

--- src/main/java/com/example/CinemaTicketAutomation/dto/SeatPosition.java
+++ src/main/java/com/example/CinemaTicketAutomation/dto/SeatPosition.java
@@ -4,7 +4,11 @@ package com.example.CinemaTicketAutomation.dto;
  * Bir koltuğun satır ve sütun pozisyonunu temsil eden değişmez (immutable) veri nesnesi.
  * Bu kayıt, Seat servisinde koltuk pozisyonlarını belirlemede kullanılır.
  */
-public record SeatPosition(String row, int column) {
+public record SeatPosition(
+        String row,
+        int column
+)
+{
     @Override
     public String toString() {
         return row + column;
